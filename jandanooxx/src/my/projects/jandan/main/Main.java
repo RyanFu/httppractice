6f30e2a33ecff62d30b452d6c1764bf539fc7e8e
jabbby <814216440@qq.com>
2013-05-06 19:10:55 +0800

    crawl the pic of ooxx in jiandan.net,, demos from oschina

---
+++ jandanooxx/src/my/projects/jandan/main/Main.java
@@ -0,0 +1,16 @@
+package my.projects.jandan.main;
+
+import my.projects.jandan.thread.ThreadPoolManager;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hqtc
+ * Date: 13-5-6
+ * Time: 下午12:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] agrs) {
+        new ThreadPoolManager();
+    }
+}
